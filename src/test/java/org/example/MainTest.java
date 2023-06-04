3823079d5245d6f08825407fbd858cc9dec3ffbe
Oleksandr <oleksandr.jr@gmail.com>
2023-06-04 16:19:16 +0200

    Test Report 3 test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -233,7 +233,7 @@ class MainTest {
 
         @DisplayName("[ENCRYPT] Simple letters encoding")
         @ParameterizedTest
-        @CsvSource({"А, 1, Б",
+        @CsvSource({"А, 1, Г",
                 "а, 1, б",
                 "А, 32, Я",
                 "а, 32, я",})
